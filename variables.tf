35bc90058a4c3c854207233bacfa744c6fcbfa8a
Vis-Rodrigues <vi_ale10@hotmail.com>
2022-03-18 03:25:56 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,5 +5,5 @@ variable "bucket_name" {
 
 variable "cloudfront_oai_arn" {
   type        = string
-  default     = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E1FZQP5DTOTQXX"
+  default     = "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E1TIRD3CYMB7C1"
 }
